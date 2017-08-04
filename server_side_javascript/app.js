42a74346bd49e4506f50ad1771fe35eea54eb2b8
Jangre <dlatjdwn8824@naver.com>
2017-08-04 19:35:53 +0900

    first commit

---
+++ server_side_javascript/app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var app = express();
+app.locals.pretty = true;
+app.set('view engine', 'jade');
+app.set('views', './views');
+app.use(express.static('public'));
+app.get('/template', function(req, res) {
+  res.render('temp', {
+    time: Date(),
+    _title: 'Jade'
+  });
+});
+app.get('/', function(req, res) {
+  res.send('Hello homepage');
+});
+app.get('/dynamic', function(req, res) {
+  var lis = '';
+  for (var i = 0; i < 5; i++) {
+    lis = lis + '<li>conding</li>';
+  }
+  var time = Date();
+  var output = `
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+  </head>
+
+  <body>
+    Hello dynamic!!
+    <ul>
+    ${lis}
+    </ul>
+    ${time}
+  </body>
+
+  </html>
+  `;
+  res.send(output);
+});
+app.get('/route', function(req, res) {
+  res.send('Hello Router, <img src="/route.png">');
+});
+app.get('/login', function(req, res) {
+  res.send('Login please');
+});
+app.listen(1337, function() {
+  console.log("Connected 1337 port!");
+});
