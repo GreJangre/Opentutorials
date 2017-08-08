3130aa07767641c02455a06a39f29982640b4c99
Jangre <dlatjdwn8824@naver.com>
2017-08-08 21:12:19 +0900

    'post'방식으로 전달

--- server_side_javascript/app.js
+++ server_side_javascript/app.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.get('/form', function(req, res) {
+  res.render('form');
+});
+app.get('/form_receiver', function(req, res) {
+  var title = req.query.title;
+  var description = req.query.description;
+  res.send(title + ',' + description);
+});
+app.post('/form_receiver', function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send(title + ',' + description);
+});
 app.get('/topic/:id', function(req, res) {
   var topics = [
     'Javascript is ...',
@@ -18,7 +35,7 @@ app.get('/topic/:id', function(req, res) {
   `;
   res.send(output);
 });
-app.get('/topic/:id/:mode', function(req, res) {
+app.get('/topic/:id/:mod', function(req, res) {
   res.send(req.params.id + ',' + req.params.mode);
 });
 app.get('/template', function(req, res) {
